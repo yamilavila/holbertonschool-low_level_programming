2be8f88545d6fd2a7c47440faea2989beea61407
yamilavila <yamilpra@hotmail.com>
2022-02-03 20:33:40 +0000

    some text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-  puts("\"Programming is like building a multinlingual puzzle");
-  return (0);
+puts("\"Programming is like building a multinlingual puzzle");
+return (0);
 }
