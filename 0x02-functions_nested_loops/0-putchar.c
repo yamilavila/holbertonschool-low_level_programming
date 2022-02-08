b15e131a95fbe9616d6abdba9e60edf492160510
yamilavila <yamilpra@hotmail.com>
2022-02-08 18:02:18 +0000

    putchar for single char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main- print _putchar
-* Return: 0
-*/
+ * main- print _putchar
+ * Return: 0
+ */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
