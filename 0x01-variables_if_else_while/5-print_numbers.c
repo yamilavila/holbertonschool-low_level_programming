71a3027431ae460e391909fab7fb995c5b41922b
yamilavila <yamilpra@hotmail.com>
2022-02-08 00:40:16 +0000

    1-9 with 1 printf 1 putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- print 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%i", i);
+	}
+	putchar ('\n');
+	return (0);
+}
