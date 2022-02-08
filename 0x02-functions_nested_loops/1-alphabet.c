e8d4f606b9e6766788782e05f0efd4a3c0de6018
yamilavila <yamilpra@hotmail.com>
2022-02-08 20:22:55 +0000

    Prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * prototype: print_alphabet- lowercase
+ * Desription: ANSII table 97-122
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122, i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
