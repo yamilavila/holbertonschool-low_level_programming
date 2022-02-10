eb28d4abb60dc55c2d4c4154b505d0a3f588e8f8
yamilavila <yamilpra@hotmail.com>
2022-02-10 02:02:15 +0000

    return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - 2 integer and return result
+ * @n: first number to add
+ * @m: second number to add
+ * REturn: result
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
