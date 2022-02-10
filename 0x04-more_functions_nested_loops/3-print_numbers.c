7371dd5434e488f4e1c36117ff57167f445efdd4
yamilavila <yamilpra@hotmail.com>
2022-02-10 23:39:45 +0000

    0 to 9 with 2 putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
