a828a38db7e6053bab1e1906b73898a0b661a013
yamilavila <yamilpra@hotmail.com>
2022-02-12 01:26:47 +0000

    simple for printing _

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
  * print_line - print lines
+ * @n: integer
  * Return: 0
  */
 void print_line(int n)
 {
 	int e;
 
-	for (e = '0'; e < n; e++)
-		_putchar('_');
-	_putchar('\n');
+	if (n > 0)
+	{
+		for (e = '0'; e < n; e++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
