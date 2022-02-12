16c3fd7387d2cdbe95f4767bd6da58724caa9248
yamilavila <yamilpra@hotmail.com>
2022-02-12 01:09:56 +0000

    3 variables 1o lines 14 char

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 0 -14
+ * @c: int
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int check;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			check = num2 / 10;
+			if (check >= 1)
+			{
+				_putchar(check + '0');
+			}
+			_putchar((num2 % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
