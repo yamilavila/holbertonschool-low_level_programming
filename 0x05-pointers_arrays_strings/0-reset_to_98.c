1622106d78303d5c6c9fb2e3a7326deb1988da0a
yamilavila <yamilpra@hotmail.com>
2022-02-14 21:26:25 +0000

    assign value to parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to int as a parameter and update value
+ * @n: parameter to be value
+ * Return: nada
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
