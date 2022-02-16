24a9f930421665a10b5e8e686120a0fa5dcb63af
yamilavila <yamilpra@hotmail.com>
2022-02-16 03:17:36 +0000

    print all characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - print every other char of a string
+ * @str: pointer to string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
