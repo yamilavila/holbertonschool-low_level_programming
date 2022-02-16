9225acf154f81ac71f28d7435580dac931449ced
yamilavila <yamilpra@hotmail.com>
2022-02-16 03:49:09 +0000

    copy string including null byte

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string including null byte
+ * @dest: buffer pointed to
+ * @src: points to string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
