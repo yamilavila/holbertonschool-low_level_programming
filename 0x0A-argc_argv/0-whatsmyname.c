9b1bd8bb4df42aca8dbe236274f156f4b630d2d4
yamilavila <yamilpra@hotmail.com>
2022-02-28 13:04:39 +0000

    argument counter & vector

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - print program
+ * main - print program name
  * @argc: argument counter
  * @argv: argument vector
  * Return: 0
  */
 
-int main(int arg __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
