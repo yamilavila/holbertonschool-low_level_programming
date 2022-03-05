f34a3691d792c5a9d9e6a796d82972224ac397ab
yamilavila <yamilpra@hotmail.com>
2022-03-05 00:59:56 +0000

    Prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,7 +10,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 /*2-calloc.c*/
-void *_calloc(unsignd int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 /*3-array_range.c*/
 int *array_range(int min, int max);
