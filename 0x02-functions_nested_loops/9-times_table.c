ac85acb89c01adc9d8ef0426e0ea70855b93fa36
yamilavila <yamilpra@hotmail.com>
2022-03-06 15:01:00 +0000

    print the 9 table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 table
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int z;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (b == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			z = a * b;
+			_putchar(',');
+			_putchar(z + 48);
+			if (z >= 10)
+			{
+				_putchar(z / 10 + 48);
+				_putchar(z % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(z + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
