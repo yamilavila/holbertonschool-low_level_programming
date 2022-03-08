a0b88c77b66ca61fa6c1244b1c0d12ed12921eec
yamilavila <yamilpra@hotmail.com>
2022-03-08 21:21:13 +0000

    structures with 3 elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+* struct dog - structure of the dog info
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Return: 0
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif
