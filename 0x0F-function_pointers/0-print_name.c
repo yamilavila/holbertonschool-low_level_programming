0022930829e107c8b888ca04f9238b2561a86a8c
yamilavila <yamilpra@hotmail.com>
2022-03-09 17:06:18 +0000

    updating

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -8,6 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	
 }
