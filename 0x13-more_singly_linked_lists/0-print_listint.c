f96ba9ee5959ccc1b4760a065b82219f8e90a98f
yamilavila <yamilpra@hotmail.com>
2022-03-22 04:09:24 +0000

    unfinish

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elments of a list
+ * @h: points the list
+ * Return: nodes
+ */
+
+size_t print_listint(const listing_t *h)
+{
+	size_t n = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+		{
+		printf("%d\n", h->n);
+		h = h->next;
+			n++;
+		}
+	return (n);
+}
