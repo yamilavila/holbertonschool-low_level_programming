9821ac031bc4a5e096df4dad7eac4580e818aea5
yamilavila <yamilpra@hotmail.com>
2022-03-29 02:09:33 +0000

    Prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,5 +20,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /* 3-cp.c */
-
+int main(int argc, char *argv[]);
 #endif
