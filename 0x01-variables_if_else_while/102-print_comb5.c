d001c9d8d2cf9ac7cad0b31e09f8790bd55e6bfd
yamilavila <yamilpra@hotmail.com>
2022-04-13 00:24:10 +0000

    fix it

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	int a = 0;
-	int b = 0;
+	int a;
+	int b;
 
-	for (; a < 100; a++)
+	for (a = 0; a < 100; a++)
 	{
-		for (; b < 100; b++)
+		for (b = 0; b < 100; b++)
 		{
 			if (a < b && a != b)
 			{
