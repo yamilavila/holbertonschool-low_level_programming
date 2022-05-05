da411042aee67e6616fb3a4693048df32533d3de
yamilavila <yamilpra@hotmail.com>
2022-05-05 22:14:41 +0000

    Task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - print all elements of a doubly list
+ * @h: head node
+ * Return: size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t cuenta = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cuenta++;
+	}
+	return (cuenta);
+}
