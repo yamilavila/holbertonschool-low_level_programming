f9686b9cbd998e29b31d541ad7d484fa155cb968
yamilavila <yamilpra@hotmail.com>
2022-05-06 23:33:18 +0000

    No compila

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add a new node at the beginning
+ * @head: first node
+ * @n: new node
+ * Return: the address of the new element or NULL if it failed
+ */
+
+dlistint_t *add_dnodeinti_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *tempo;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	
+	new->next = NULL;
+	new->n = n;
+
+	if (!*head)
+	{
+		new-> prev = NULL;
+		*head = new;
+		return (new);
+	}
+	
+	tempo = *head;
+
+	while (tempo->next)
+		tempo = tempo->next;
+
+	new-> prev = tempo;
+	tempo->next = new;
+
+	return (new);
+}
