535b66e9785b64dd0da22dab661438d3cc590507
yamilavila <yamilpra@hotmail.com>
2022-05-07 04:10:51 +0000

    adv1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * printBeforeMain - print the message before main is executed
+ */
+
+void printBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
