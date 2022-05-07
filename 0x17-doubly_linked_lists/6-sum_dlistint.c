f82f0315f33f50679215692f443352dcf6604db7
yamilavila <yamilpra@hotmail.com>
2022-05-07 01:03:16 +0000

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/*
+ * sum_dlistint - sum of all the data (n) of a dlistint linked list
+ * @head: first node
+ * Return: 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	size_t reslt = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		reslt += head->n;
+		head = head->next;
+	}
+	return (reslt);
+}
