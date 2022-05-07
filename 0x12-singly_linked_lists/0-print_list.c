2838f9b2a4a47beaa5c91ed5aee37ab6d41d240d
yamilavila <yamilpra@hotmail.com>
2022-05-07 03:01:32 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nod = 0;
+	int cuenta = 0;
 
-	if (h == NULL)
-		return (nod);
-	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-
+		}
+		cuenta++;
 		h = h->next;
-
-		nod++;
 	}
-
-	return (nd);
+	return (cuenta);
 }
