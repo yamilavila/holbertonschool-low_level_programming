481ae147c2419490ee361c43f8a07cc7027ff28c
yamilavila <git@github.com>
2022-06-22 00:39:32 +0000

    Task 1: djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 algorithm implementation
+ * @str: string use to generate has value
+ * Return: has value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
