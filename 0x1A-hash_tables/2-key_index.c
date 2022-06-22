3989aaaf28470d7e5621ccba581da141452c5adc
yamilavila <git@github.com>
2022-06-22 02:33:21 +0000

    Task 2 key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,7 @@
  * key_index - gives you the index of a key
  * @key: is the key
  * @size: size of the array
- * Returns: index at  which the key/value pair should be stored in the array 
+ * Returns: index at  which the key/value pair should be stored in the array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
