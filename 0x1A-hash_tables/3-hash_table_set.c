90d7e8e0ce822c16121a8c74eed764f136ed3592
yamilavila <git@github.com>
2022-06-22 01:53:35 +0000

    Task 3: >>> ht]'betty'] ='cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = malloc(sizeof(hash_table_t));
 		if (node == NULL)
 			return (0);
-		node->key = strdup(key);
+		node->key = strdup(key); node->value = strdup(value);
 		node->next = ht->array[indx];
 		ht->array[indx] = node;
 	}
