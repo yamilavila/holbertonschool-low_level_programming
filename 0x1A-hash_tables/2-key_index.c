58643a4d3464a38d33b4991db8816bed2c24cd7a
yamilavila <git@github.com>
2022-07-10 13:58:52 +0000

    Betty fix typo

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,13 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives you the index of a key
+ * key_index -Function gives you the index of a key.
+ *
  * @key: is the key
  * @size: size of the array
- * Returns: index at  which the key/value pair should be stored in the array
+ * Return: index at  which the key/value pair should be stored in the array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	if (key == NULL || size == 0)
+	{
+		return (0);
+	}
 	return (hash_djb2(key) % size);
 }
