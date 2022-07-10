66b84669c6a4aa8d9d85aef2c72bb25e9272e3ef
yamilavila <git@github.com>
2022-07-10 14:34:12 +0000

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -Function prints table
+ * @ht: hash table
+ * Return: none
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, commasymbol = 0;
+	hash_node_t *tmp_hn;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp_hn = ht->array[i];
+		while (tmp_hn != NULL)
+		{
+			if (commasymbol)
+				print(", ");
+			printf("'%s': '%s'", tmp_hn->key, tmp_hn->value);
+			tmp_hn = tmp_hn->next;
+			commasymbol = 1;
+		}
+	}
+	printf("}\n");
+}
