0b2340c87cb37ee94e56fcdaef6ca58565767bd6
yamilavila <yamilpra@hotmail.com>
2022-08-30 14:05:19 -0400

    Task debuged

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array != NULL)
 		for (index = 0; index < size; index++)
 		{
-			printf("Value checked array{%lu] = [%d]\n", index, array[index]);
+			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 			if (array[index] == value)
 				return (index);
 		}
