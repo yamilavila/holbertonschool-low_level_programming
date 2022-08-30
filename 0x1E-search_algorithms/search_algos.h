f18b159ce742b0c388bfb801810519be787cbcc9
yamilavila <yamilpra@hotmail.com>
2022-08-30 10:20:44 -0400

    Prototypes & Functions

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifdef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 0.Linear search*/
+int linear_search(int *array, size_t, int value);
+
+/* 1.Binary search */
+ int binary_search(int *array, size_t size, int value);
+
+#endif
