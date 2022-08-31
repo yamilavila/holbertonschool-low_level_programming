86cbaa6a915e074a658486687fd1e115044576aa
yamilavila <yamilpra@hotmail.com>
2022-08-30 20:43:25 -0400

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,6 @@
 int linear_search(int *array, size_t, int value);
 
 /* 1.Binary search */
- int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
