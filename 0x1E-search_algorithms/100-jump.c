1cb952eac78ca72e1f6bcea8fedf1326d883d7ce
yamilavila <yamilpra@hotmail.com>
2022-08-31 20:19:04 -0400

    Operator typo, line 31

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 
 	while (move <= index)
 	{
-		printf("Value checked array [%lu] and [%d]\n", move, array[move]);
+		printf("Value checked array [%lu] = [%d]\n", move, array[move]);
 		if (array[move] == value)
 			return (move);
 		move++;
